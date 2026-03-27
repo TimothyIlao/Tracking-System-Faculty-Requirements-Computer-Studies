cb80c13f8a4a4f8c742416a3495f6ad25131515a
Timothy James Ilao <timothyjamesilao11@gmail.com>
2026-03-27 21:29:59 +0800

    Delete loginform.py

--- loginform.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import RadioField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
-
-class Logindetails(FlaskForm):
-    username = StringField(label = "", validators=[DataRequired()]) # cinopypaste ko naalng para mabilis
-    password = PasswordField(label = "", validators=[DataRequired()])
-    submit = SubmitField("Login")
